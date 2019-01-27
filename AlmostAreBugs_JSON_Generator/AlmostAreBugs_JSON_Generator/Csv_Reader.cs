3243f9aa819f0f5b1e007d1a0dbb8ee83ded5f19
psb2942 <psb2942@gmail.com>
2019-01-27 23:45:32 +0900

    Csv Reader를 짰습니다.

--- AlmostAreBugs_JSON_Generator/AlmostAreBugs_JSON_Generator/Csv_Reader.cs
+++ AlmostAreBugs_JSON_Generator/AlmostAreBugs_JSON_Generator/Csv_Reader.cs
@@ -6,20 +6,61 @@ using System.Threading.Tasks;
 using System.IO;
 namespace AlmostAreBugs_JSON_Generator {
     class Csv_Reader {
+        public readonly string[] fileList = {"asdf"};
+        public readonly string[][][] dataRead;
+        readonly StreamReader streamReader;
         public Csv_Reader() {
+
             string path = System.IO.Directory.GetCurrentDirectory();
             DirectoryInfo directoryInfo = new DirectoryInfo( path );
-            foreach( var file in directoryInfo.GetFiles() ) {
-                if( file.Extension == ".csv" ) {
 
-                    StreamReader streamReader = new StreamReader( file.FullName, Encoding.Unicode ); //일까요.
-                    while( streamReader.EndOfStream ) {
+            var validFiles = from file in directoryInfo?.GetFiles()
+                             where file.Extension == ".csv" && file.Name.Contains( "Data" )
+                             select file;
 
+            if( validFiles == null )
+                throw new FileNotFoundException();
+
+            dataRead = new string[ validFiles.Count() ][][];
+            foreach( var file in validFiles ) {
+                for( int i = 0; i < fileList.Length; i++ ) {
+                    if( file.Name.Contains( fileList[ i ] ) ) {
+                        streamReader = new StreamReader( file.FullName, Encoding.Unicode ); //일까요.
+                        ReadDataInStreamReader( streamReader, out dataRead[ i ] );
                     }
-                    //뭔갈 함.
                 }
             }
         }
-        
+        public string[][] Read(string str ) {
+            for( int i = 0; i < fileList.Length; i++ )
+                if( str == fileList[ i ] )
+                    return dataRead[ i ];
+            System.Console.WriteLine( "유요하지 않은 데이터 이름입니다: {1}", str );
+            throw new ArgumentException();
+        }
+        private void ReadDataInStreamReader( StreamReader streamReader, out string[][] dataOutput ) {
+            
+            int argnum;
+            if( streamReader.EndOfStream || ( argnum = streamReader.ReadLine().Split( ',' ).Length ) == 0 ) 
+                throw new FormatException();
+            dataOutput = new string[ argnum ][];
+            List<string> templist = new List<string>();
+
+            while( !streamReader.EndOfStream )
+                templist.Add( streamReader.ReadLine() );
+
+            for(int i=0; i< argnum; i++ )
+                dataOutput[ i ] = new string[ templist.Count ];
+
+            for(int i=0; i<templist.Count; i++ ) {
+                if(templist[i].Length!= argnum ) {
+                    Console.WriteLine( "{1} 스트림의 {2}번째 줄에서 형식오류가 발생했습니다.", streamReader.ToString(),i );
+                    throw new FormatException();
+                }
+                for(int j=0; j< argnum; j++ ) {
+                    dataOutput[ j ][ i ] = templist[ i ].Split( ',' )[ j ];
+                }
+            }
+        }
     }
 }
