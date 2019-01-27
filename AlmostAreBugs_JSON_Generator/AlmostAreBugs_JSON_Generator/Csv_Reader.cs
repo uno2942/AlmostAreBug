fea5d968199823b3a90ebf8f08be3fddcb7cb1e3
uno2942 <psb2942@gmail.com>
2019-01-27 16:51:07 +0900

    설정 파일(JSON 형식으로 생각하고 있습니다.)를 쓰고 읽는 C# 프로젝트입니다.

---
+++ AlmostAreBugs_JSON_Generator/AlmostAreBugs_JSON_Generator/Csv_Reader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace AlmostAreBugs_JSON_Generator {
+    class Csv_Reader {
+        public Csv_Reader() {
+            string path = System.IO.Directory.GetCurrentDirectory();
+            DirectoryInfo directoryInfo = new DirectoryInfo( path );
+            foreach( var file in directoryInfo.GetFiles() ) {
+                if( file.Extension == ".csv" ) {
+
+                    StreamReader streamReader = new StreamReader( file.FullName, Encoding.Unicode ); //일까요.
+                    while( streamReader.EndOfStream ) {
+
+                    }
+                    //뭔갈 함.
+                }
+            }
+        }
+        
+    }
+}
---
+++ AlmostAreBugs_JSON_Generator/AlmostAreBugs_JSON_Generator/JSON_Gemerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace AlmostAreBugs_JSON_Generator {
+    class JSON_Gemerator {
+        
+    }
+}
